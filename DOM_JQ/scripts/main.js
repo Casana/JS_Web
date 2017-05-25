0abc4984214b09b24933a1aaa76514625a08958d
Casana <carlos.martinez.casana@tajamar365.com>
2017-05-25 20:30:28 +0200

    clase jueves 25

---
+++ DOM_JQ/scripts/main.js
@@ -0,0 +1,21 @@
+	$(function() {
+
+
+
+	        $('#addBtn').click(function() {
+
+
+	                $('ul#lista').append("<li>"
+	                    "</li>");
+	            }) // fin de la funcion anonima y del Evento click
+
+
+	    }) // fin del onload
+
+	//function respondeBoton() {
+	//   $('ul#lista').append("<li>" + prompt('Introduce un nuevo valor', '') + "</li>");
+	//}
+
+	//$(function () {
+	//    $('#addBtn')[0].onclick = respondeBoton;
+	//}) // fin del onload
