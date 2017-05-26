0a67c3e844cac64a255f8f2042cad5febd31c469
Casana <carlos.martinez.casana@tajamar365.com>
2017-05-26 21:37:39 +0200

    clave viernes

---
+++ Formulario_mio/scripts/formulario.js
@@ -0,0 +1,16 @@
+function main() {
+
+    var aCursos = ["Informatica", "Derecho", "Empresariales", "Construccion"]
+    var oAsignaturas = {
+        "Informatica": ["a", "b", "c", "d", "f", "g"],
+        "Derecho": ["a", "b", "c", "d", "f", "g"],
+        "Empresariales": ["a", "b", "c", "d", "f", "g"],
+        "Construccion": ["a", "b", "c", "d", "f", "g"]
+    };
+
+    var sSelectedCurso;
+    var sSelectedAsignatura;
+    // nodos del DOM
+    var domSelectCursos = document.querySelector("#cursos")
+
+}
