9ef786c047449cf49a809a06a2ae72975d6e5c65
Casana <carlos.martinez.casana@tajamar365.com>
2017-05-31 15:13:47 +0200

    Casa miercoles final

--- Formulario_mio/scripts/formulario.js
+++ Formulario_mio/scripts/formulario.js
@@ -1,3 +1,8 @@
 window.onload = function() {
-    document.getElementById("fecha").innerHTML = fechaActual("Madrid");
+    document.getElementById("Correo").innerHTML = function(fechaHoy) {
+        var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+        var f = new Date();
+        document.write(f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
+    }
+    document.getElementById("Correo").innerHTML = fechaHoy()
 }
