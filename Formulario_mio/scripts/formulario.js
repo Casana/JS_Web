d86111e70a41b7d917277d7092a46ec49e05121f
Casana <carlos.martinez.casana@tajamar365.com>
2017-05-31 12:21:30 +0200

    Casa Miercoles (restauración repositorio)

--- Formulario_mio/scripts/formulario.js
+++ Formulario_mio/scripts/formulario.js
@@ -1,16 +1,3 @@
-function main() {
-
-    var aCursos = ["Informatica", "Derecho", "Empresariales", "Construccion"]
-    var oAsignaturas = {
-        "Informatica": ["a", "b", "c", "d", "f", "g"],
-        "Derecho": ["a", "b", "c", "d", "f", "g"],
-        "Empresariales": ["a", "b", "c", "d", "f", "g"],
-        "Construccion": ["a", "b", "c", "d", "f", "g"]
-    };
-
-    var sSelectedCurso;
-    var sSelectedAsignatura;
-    // nodos del DOM
-    var domSelectCursos = document.querySelector("#cursos")
-
+window.onload = function() {
+    document.getElementById("fecha").innerHTML = fechaActual("Madrid");
 }
