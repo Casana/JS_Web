c515272024ef77cc041d33b121c5655ccc659c0b
Casana <carlos.martinez.casana@tajamar365.com>
2017-05-31 21:35:18 +0200

    Final clase Miercoles 31. Ejercicio Mayusculas-minusculas y modificación formulario examen (reloj actualiza)

--- Formulario_mio/scripts/formulario.js
+++ Formulario_mio/scripts/formulario.js
@@ -6,3 +6,17 @@ window.onload = function() {
     }
     document.getElementById("Correo").innerHTML = fechaHoy()
 }
+
+function main()
+
+var domFormulario = document.getElementById("formulario");
+var domClave1 = document.getElementById("pass1");
+var domClave2 = document.getElementById("pass2");
+
+var msg = "";
+if (clave1.value != clave2.value) {
+    msg = "Las contraseñas no coinciden"
+}
+domPass2.setCustomValidity(msg)
+
+main()
