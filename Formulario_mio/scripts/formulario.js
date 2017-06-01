bdeab9ba4e4e180e9b0a53686ea8d75e96f0628f
Casana <carlos.martinez.casana@tajamar365.com>
2017-06-01 14:58:28 +0200

    Villanueva jueves (actualizacion formulario)

--- Formulario_mio/scripts/formulario.js
+++ Formulario_mio/scripts/formulario.js
@@ -1,22 +1,72 @@
-window.onload = function() {
-    document.getElementById("Correo").innerHTML = function(fechaHoy) {
-        var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-        var f = new Date();
-        document.write(f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
+function main() {
+
+    var aCursos = ["HTML", "CSS", "JavaScript", "PHP"];
+    var oEspecialidades = {
+        "HTML": ["Diseño de webs", "Mantenimiento de webs", "Orientado a objetos", "Diseño libre"],
+        "CSS": ["Apariencia de webs", "Apariencia de aplicaciones", "Creación de estilos", "Bootstrap"],
+        "JavaScript": ["Core", "Orientado a webs", "jQuery", "Angular"],
+        "PHP": ["XAMPP", "LAMP", "WAMP", "MAMP", "UwAmp"]
+    };
+    var sSeleccionCurso;
+    var sSeleccionEspec;
+
+    // elementos del DOM
+    var domSelecCurso = document.querySelector("#cursos");
+    var domSelecEspec = document.querySelector("#especialidades");
+
+    //manejadores
+    domSelecCurso.addEventListener("change", cambioCurso);
+    domSelecEspec.addEventListener("change", cambioEspecialidades);
+    domSelecEspec.disabled = true;
+
+    cargarSelect(domSelecCursos, aCursos);
+
+    function cambioEspecialidades(oE) {
+        var domSelect = oE.currentTarget;
+        if (domSelect.childElementCount > oEspecialidades[sSeleccionCurso].length) {
+            domSelect.removeChild(domSelect.children[0]);
+        }
+        var nSelect = domSelect.selectedIndex;
+        var domOption = domSelect.options[nSelect]
+        sSelectedEspec = domOption.text;
     }
-    document.getElementById("Correo").innerHTML = fechaHoy()
+
+    function cambioCurso(oE) {
+        var domSelect = oE.currentTarget;
+        if (domSelect.childElementCount > aCursos.length) {
+            domSelect.removeChild(domSelect.children[0]);
+        }
+        let nSelect = domSelect.selectedIndex;
+        let domOption = domSelect.options[nSelect];
+        sSeleccionCurso = domOption.text;
+
+        cargarSelect(domSelecEspecialidades, oEspecialidades[sSelectedCurso]);
+        domSelecEspecialidades.disabled = false;
+    }
+
+    function cargarSelect(oDom, aDatos) {
+        oDom.innerHTML = "<option> </option>";
+        for (var i = 0; i < aDatos.length; i++) {
+            oDom.innerHTML += "<option>" + aDatos[i] + "</option>";
+        }
+    }
 }
 
-function main()
 
-var domFormulario = document.getElementById("formulario");
-var domClave1 = document.getElementById("pass1");
-var domClave2 = document.getElementById("pass2");
 
-var msg = "";
-if (clave1.value != clave2.value) {
-    msg = "Las contraseñas no coinciden"
-}
-domPass2.setCustomValidity(msg)
 
-main()
+
+
+// Comprobar que ambas contraseñas coinciden:
+
+// var domFormulario = document.getElementById("formulario");
+// var domClave1 = document.getElementById("pass1");
+// var domClave2 = document.getElementById("pass2");
+
+// var msg = "";
+// if (clave1.value != clave2.value) {
+//     msg = "Las contraseñas no coinciden"
+// }
+// domPass2.setCustomValidity(msg)
+
+// main()
