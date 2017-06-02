3b0465bab92f336ccdca2c7095f581437473b371
Casana <carlos.martinez.casana@tajamar365.com>
2017-06-02 22:25:31 +0200

    Casa viernes (cambios formulario)

--- Formulario_mio/scripts/recogedatos.js
+++ Formulario_mio/scripts/recogedatos.js
@@ -36,11 +36,48 @@ class Formulario {
     }
 
     recogeDatos(oE) {
-        this.oDatos.CuentaCorreo = document.getElementById("correo").value;
+            this.oDatos.CuentaCorreo = document.getElementById("correo").value;
+            this.oDatos.Contraseña = document.getElementById("cont").value;
+            this.oDatos.Nombre = document.getElementById("name").value;
+            this.oDatos.PrimerApellido = document.getElementById("ape1").value;
+            this.oDatos.SegundoApellido = document.getElementById("ape2").value;
+            this.oDatos.DNI = document.getElementById("dni").value;
+            this.oDatos.FechaNacimiento = document.getElementById("dia" + "mes" + "año").value;
+            this.oDatos.Domicilio = document.getElementById("domic").value;
+            this.oDatos.Ciudad = document.getElementById("ciudad").value;
+            this.oDatos.CodigoPostal = document.getElementById("cp").value;
+
+            //llamada a la función que procesara el objeto presentandolo en pantalla
+            this.escribeDatos();
+            oE.preventDefault();
+            //anula el comportamiento por defecto de submit
+        } //Fin del método recogeDatos
+
+    escribeDatos() {
+            //ocultar formulario
+            document.getElementById("formul").classList.toggle("oculto")
+                //mostrar bloque div para el resultado
+            document.getElementById("resultado").classList.toggle("oculto");
+            //incorporamos a la lista cada elemento del objeto		
+            for (var i in this.oDatos) {
+                this.oParrafo.innerHTML += "<li>" + i + ": <strong>" +
+                    this.oDatos[i] + "</strong></li>";
+            };
+        } //Fin del método escribedatos	
+
+} // Fin de la clase Formulario
+
+class App {
+
+    static main() {
+
+        document.addEventListener("DOMContentLoaded",
+            function() {
+                new Formulario()
+            }, false);
     }
-}
+
+} // Fin de la clase App
 
 
-
-
-//document.getElementById("comentarios").value;
+App.main()
